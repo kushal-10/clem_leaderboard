7258970863c5c3d496697cb4f3d23c32d3b1685c
kushal-10 <kushalkoshti11@gmail.com>
2023-11-16 19:04:01 +0100

    add details and versions tab, docstrings

--- app.py
+++ app.py
@@ -5,72 +5,77 @@ import gradio as gr
 import os
 
 from src.assets.text_content import TITLE, INTRODUCTION_TEXT, LLM_BENCHMARKS_TEXT
-from src.utils import update_cols, get_prev
+from src.utils import update_cols, get_data
 
 ########### OVERALL LEADERBOARD ##################
 # Get Overall Results Dataframe
-global overall_df
-overall_df = pd.read_csv(os.path.join('versions', 'latest', 'results.csv'))
-overall_df = update_cols(overall_df)
-ALL_COLS = list(overall_df.columns)
+# global overall_df
+# overall_df = pd.read_csv(os.path.join('versions', 'latest', 'results.csv'))
+# overall_df = update_cols(overall_df)
+# ALL_COLS = list(overall_df.columns)
 
-#Default sort by clemscore
-overall_df = overall_df.sort_values(by=[ALL_COLS[1]], ascending=False)
+# #Default sort by clemscore
+# overall_df = overall_df.sort_values(by=[ALL_COLS[1]], ascending=False)
 
 
 ########## Previous VERSIONS ###########################
-dfs, names = get_prev()
+# dfs, names = get_prev()
+
+
+
+
+############################ GET LEADERBOARD DATA #############################
+DATA_PATH = 'versions'
+latest_flag = True #Set flag to iclude latest data in Details and Versions Tab
+latest_df, latest_vname, previous_df, previous_vname = get_data(DATA_PATH, latest_flag)
 
 global prev_df
-
-prev_df = dfs[0]
-
+prev_df = previous_df[0]
 def select_prev_df(name):
-    ind = names.index(name)
-    prev_df = dfs[ind]
+    ind = previous_vname.index(name)
+    prev_df = previous_df[ind]
     return prev_df
 
-
 ############# PLOTS ######################
-global plot_df
-plot_df = pd.read_csv(os.path.join('versions', 'latest', 'detailed-bench-stats.csv'))
-GAME_COLS = list(plot_df['game'].unique())
-global MODEL_COLS
-MODEL_COLS = list(plot_df['model'].unique())
+# global plot_df
+# plot_df = pd.read_csv(os.path.join('versions', 'latest', 'detailed-bench-stats.csv'))
+# GAME_COLS = list(plot_df['game'].unique())
+# global MODEL_COLS
+# MODEL_COLS = list(plot_df['model'].unique())
 
 
-def plot_line(models, game):
-    df = plot_df
-    df = df[df['metric'] == 'Main Score']
-    df = df[df['game'] == game]
-    df = df[df['model'].isin(models)]
+# def plot_line(models, game):
+#     df = plot_df
+#     df = df[df['metric'] == 'Main Score']
+#     df = df[df['game'] == game]
+#     df = df[df['model'].isin(models)]
 
-    X = df['experiment'].unique()
-    Y = []
-    labels = []
-    for m in models:
-        model_df = df[df['model'] == m]
-        y = list(model_df['mean'])
-        nan_flag = False
-        for val in y:
-            if np.isnan(val):
-                nan_flag = True
-        if not nan_flag:
-            Y.append(y)
-            labels.append(str(m))
+#     X = df['experiment'].unique()
+#     Y = []
+#     labels = []
+#     for m in models:
+#         model_df = df[df['model'] == m]
+#         y = list(model_df['mean'])
+#         nan_flag = False
+#         for val in y:
+#             if np.isnan(val):
+#                 nan_flag = True
+#         if not nan_flag:
+#             Y.append(y)
+#             labels.append(str(m))
 
-    fig = plt.figure()
-    for i in range(len(Y)):
-        if len(Y[i]) != 0:
-            plt.plot(X, Y[i], label=labels[i], marker='o')
+#     fig = plt.figure()
+#     for i in range(len(Y)):
+#         if len(Y[i]) != 0:
+#             plt.plot(X, Y[i], label=labels[i], marker='o')
     
-    title_str = "Main Scores of each experiment in " + str(game) 
-    plt.xlabel('Experiments')
-    plt.ylabel('Main Score')
-    plt.title(title_str)
-    plt.legend()
-    plt.show()
-    return fig
+#     title_str = "Main Scores of each experiment in " + str(game) 
+#     plt.xlabel('Experiments')
+#     plt.ylabel('Main Score')
+#     plt.title(title_str)
+#     plt.legend()
+#     plt.show()
+#     return fig
 
 
 ############# MAIN APPLICATION ######################
@@ -83,48 +88,48 @@ with demo:
         with gr.TabItem("🥇 Clem Leaderboard", elem_id="llm-benchmark-tab-table", id=0):
                         
             leaderboard_table = gr.components.Dataframe(
-                value=overall_df,
+                value=latest_df[0],
                 elem_id="leaderboard-table",
                 interactive=False,
                 visible=True,
             )
                 
-        with gr.TabItem("📈 Plot", id=1):
-            with gr.Row():
-                game_cols = gr.Radio(
-                    GAME_COLS, label="Select Game 🎮"
-                )
-            with gr.Row():
-                model_cols = gr.CheckboxGroup(
-                    MODEL_COLS, 
-                    label="Select Models 🤖", 
-                    value=[],
-                    elem_id="column-select",
-                    interactive=True,
-                )
+        # with gr.TabItem("📈 Plot", id=3):
+        #     with gr.Row():
+        #         game_cols = gr.Radio(
+        #             GAME_COLS, label="Select Game 🎮"
+        #         )
+        #     with gr.Row():
+        #         model_cols = gr.CheckboxGroup(
+        #             MODEL_COLS, 
+        #             label="Select Models 🤖", 
+        #             value=[],
+        #             elem_id="column-select",
+        #             interactive=True,
+        #         )
 
-            with gr.Row():
-                # Output block for the plot
-                plot_output = gr.Plot()
+        #     with gr.Row():
+        #         # Output block for the plot
+        #         plot_output = gr.Plot()
 
-            model_cols.change(
-                plot_line,
-                [model_cols, game_cols],
-                plot_output,
-                queue=True
-            )
+        #     model_cols.change(
+        #         plot_line,
+        #         [model_cols, game_cols],
+        #         plot_output,
+        #         queue=True
+        #     )
 
-            game_cols.change(
-                plot_line,
-                [model_cols, game_cols],
-                plot_output,
-                queue=True
-            )
+        #     game_cols.change(
+        #         plot_line,
+        #         [model_cols, game_cols],
+        #         plot_output,
+        #         queue=True
+        #     )
 
-        with gr.TabItem("🔄 Previous Versions", elem_id="llm-benchmark-tab-table-prev", id=2):
+        with gr.TabItem("🔄 Versions and Details", elem_id="details", id=2):
             with gr.Row():
                 ver_selection = gr.Dropdown(
-                    names, label="Select Version 🕹️", value=names[0]
+                    previous_vname, label="Select Version 🕹️", value=previous_vname[0]
                 )
             prev_table = gr.components.Dataframe(
                 value=prev_df,
--- src/utils.py
+++ src/utils.py
@@ -1,10 +1,24 @@
 import os
 import pandas as pd
 
+
 def update_cols(df: pd.DataFrame) -> pd.DataFrame:
+    '''
+    Change three header rows to a single header row
+    Args:
+        df: Raw dataframe containing 3 separate header rows
+            Remove this function if the dataframe has only one header row
+
+    Returns:
+        df: Updated dataframe which has only 1 header row instead of 3
+    '''
     default_cols = list(df.columns)
+
+    # First 4 columns are initalised in 'update', Append additional columns for games Model, Clemscore, ALL(PLayed) and ALL(Main Score)
     update = ['Model', 'Clemscore', 'All(Played)', 'All(Quality Score)']
     game_metrics = default_cols[4:]
+
+    # Change columns Names for each Game
     for i in range(len(game_metrics)):
         if i%3 == 0:
             game = game_metrics[i]
@@ -12,6 +26,7 @@ def update_cols(df: pd.DataFrame) -> pd.DataFrame:
             update.append(str(game).capitalize() + "(Quality Score)") 
             update.append(str(game).capitalize() + "(Quality Score[std])")
 
+    # Create a dict to change names of the columns
     map_cols = {}
     for i in range(len(default_cols)):
         map_cols[default_cols[i]] = str(update[i])
@@ -21,38 +36,88 @@ def update_cols(df: pd.DataFrame) -> pd.DataFrame:
 
     return df
 
-def split_cols(LIST: list):
-    PL_COLS = []
-    MS_COLS = []
-    SD_COLS = []
-    dummy = LIST[4:]
-    for i in range(len(dummy)):
-        if i%3 == 0:
-            PL_COLS.append(dummy[i])
-            MS_COLS.append(dummy[i+1])
-            SD_COLS.append(dummy[i+2])
+def process_df(df: pd.DataFrame) -> pd.DataFrame:
+    '''
+    Process dataframe - Remove repition in model names, convert datatypes to sort by "float" instead of "str"
+    Args:
+        df: Unprocessed Dataframe (after using update_cols)
+    Returns:
+        df: Processed Dataframe
+    '''
 
-    AVG_COLS = LIST[1:4]
-    SHOW_COLS = LIST[1:]
+    # Change column type to float from str
+    list_column_names = list(df.columns)
+    model_col_name = list_column_names[0]
+    for col in list_column_names:
+        if col != model_col_name:
+            df[col] = df[col].astype(float)
 
-    return SHOW_COLS, PL_COLS, MS_COLS, SD_COLS, AVG_COLS
+    # Remove repetition in model names, if any
+    models_list = []
+    for i in range(len(df)):
+        model_name = df.iloc[i][model_col_name]
+        splits = model_name.split('--')
+        splits = [split.replace('-t0.0', '') for split in splits] # Comment to not remove -t0.0
+        if splits[0] == splits[1]:
+            models_list.append(splits[0])
+        else:
+            models_list.append(splits[0] + "--" + splits[1])
+    df[model_col_name] = models_list
+    
+    return df
 
+def get_data(path, flag):
+    '''
+    Get a list of all version names and respective Dataframes 
+    Args: 
+        path: Path to the directory containing CSVs of different versions -> v0.9.csv, v1.0.csv, ....
+        flag: Set this flag to include the latest version in Details and Versions tab
+    Returns: 
+        latest_df: singular list containing dataframe of the latest version of the leaderboard with only 4 columns 
+        latest_vname: list of the name of latest version 
+        previous_df: list of dataframes for previous versions (can skip latest version if required) 
+        previous_vname: list of the names for the previous versions (INCLUDED IN Details and Versions Tab)
 
-def get_prev():
-    list_versions = os.listdir(os.path.join('versions', 'previous'))
-    csv_file_names = [os.path.splitext(file)[0] for file in list_versions if file.endswith('.csv')]
-    dfs = []
-    version_names = []
-    for i in range(len(list_versions)):
-        path = os.path.join('versions', 'previous', list_versions[i])
-        df = pd.read_csv(path)
-        df = update_cols(df)
-        df = df.sort_values(by=['Clemscore'], ascending=False)
-        dfs.append(df)
-        version_names.append(csv_file_names[i])
+    '''
+    # Check if Directory is empty
+    list_versions = os.listdir(path)
+    if not list_versions:
+        print("Directory is empty")
 
-    print(dfs, version_names)
-    print(version_names.index('v0.9'))
-    return dfs, version_names
+    else:
+        files = [file for file in list_versions if file.endswith('.csv')]
+        files.sort(reverse=True)
+        file_names = [os.path.splitext(file)[0] for file in files]
 
-get_prev()
+        DFS = []
+        for file in files:
+            df = pd.read_csv(os.path.join(path, file))
+            df = update_cols(df) # Remove if by default there is only one header row
+            df = process_df(df) # Process Dataframe
+            df = df.sort_values(by=list(df.columns)[1], ascending=False) # Sort by clemscore
+            DFS.append(df)
+
+        # Only keep relavant columns for the main leaderboard
+        latest_df_dummy = DFS[0]
+        all_columns = list(latest_df_dummy.columns)
+        keep_columns = all_columns[0:4]
+        latest_df_dummy = latest_df_dummy.drop(columns=[c for c in all_columns if c not in keep_columns])
+
+        latest_df = [latest_df_dummy]
+        latest_vname = [file_names[0]]
+        previous_df = []
+        previous_vname = []
+        for df, name in zip(DFS, file_names):
+            previous_df.append(df)
+            previous_vname.append(name) 
+        
+        if not flag:
+            previous_df.pop(0)
+            previous_vname.pop(0)
+
+        print(latest_df[0].columns, previous_df[0].columns)
+        return latest_df, latest_vname, previous_df, previous_vname
+    
+    return None
+
+# get_data('versions', True)
